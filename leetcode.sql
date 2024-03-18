e0fc94e0660e01c29a96bd7ff889d90dbc284278
Antanina Druzhkina <totora246@gmail.com>
2024-03-18 15:27:18 +0400

    1378. Replace Employee ID With The Unique Identifier

---
+++ leetcode.sql
@@ -0,0 +1,6 @@
+-- 1378. Replace Employee ID With The Unique Identifier
+-- https://leetcode.com/problems/replace-employee-id-with-the-unique-identifier/description/?envType=study-plan-v2&envId=top-sql-50
+SELECT e2.unique_id, e1.name 
+FROM Employees AS e1
+LEFT JOIN EmployeeUNI AS e2
+ON e1.id = e2.id
