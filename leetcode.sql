4587251fdd61c82c273afb7d1322e4e12b82df69
Antanina Druzhkina <totora246@gmail.com>
2024-03-18 15:50:57 +0400

    1581. Customer Who Visited but Did Not Make Any Transactions

--- leetcode.sql
+++ leetcode.sql
@@ -12,3 +12,12 @@ SELECT p.product_name, s.year, s.price
 FROM Sales as s
 INNER JOIN Product AS p
 ON s.product_id = p.product_id 
+
+-- 1581. Customer Who Visited but Did Not Make Any Transactions
+-- https://leetcode.com/problems/customer-who-visited-but-did-not-make-any-transactions/description/?envType=study-plan-v2&envId=top-sql-50
+SELECT v1.customer_id, COUNT(*) as count_no_trans 
+FROM Visits AS v1
+LEFT JOIN Transactions AS t1
+ON v1.visit_id = t1.visit_id
+WHERE t1.visit_id IS NULL 
+GROUP BY v1.customer_id
